1a7d0c28235511a137e9bdfe24efae01c6d8eb97
Student <example@example.com>
2020-01-26 14:59:01 -0800

    Add number inputs and jQuery logic to retrieve number values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 //Business Logic
 
 //UI Logic
+$(document).ready(function() {
+  $("button#submit").on("click", function() {
+    let num1 = $("#num1").val();
+    let num2 = $("#num2").val();
+    let num3 = $("#num3").val();
+    //console.log(num1, num2, num3);
+    
+    if (num1 === num2 && num2 === num3) {
+      
+    }
+    
+
+    event.preventDefault();
+  });
+
+});
