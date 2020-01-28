fefcc1c4ef82114575be170948fa3fe83190941a
Winter Runion <winterrunion@gmail.com>
2020-01-28 00:26:43 -0800

    add functions to validate user input and verify that the inputs create a valid triangle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,64 @@
 //Business Logic
+function checkTriangle(num1, num2, num3) {
+
+  let condition1;
+  let condition2;
+  let condition3;
+
+  if (num1 + num2 > num3) {
+    condition1 = true;
+  } 
+
+  if (num2 + num3 > num1) {
+    condition2 = true;
+  }
+
+  if (num3 + num1 > num2) {
+    condition3 = true;
+  }
+
+  if (condition1 === true && condition2 === true && condition3 === true) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
+function checkInput(num1, num2, num3) {
+  if (num1 && num2 && num3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 //UI Logic
 $(document).ready(function() {
   $("button#submit").on("click", function() {
-    let num1 = $("#num1").val();
-    let num2 = $("#num2").val();
-    let num3 = $("#num3").val();
+    //Get the inputted user values and assign them to variables
+    let num1 = parseInt($("#num1").val());
+    let num2 = parseInt($("#num2").val());
+    let num3 = parseInt($("#num3").val());
     //console.log(num1, num2, num3);
     
-    if (num1 === num2 && num2 === num3) {
-      
+    //Check to make sure that the user actually entered positive integers, and that those integers form a valid triangle, and assign those booleans to variables
+    let isValidInput = checkInput(num1, num2, num3);
+    let isTriangle = checkTriangle(num1, num2, num3);
+
+    if (isValidInput && isTriangle) {
+      if (num1 === num2 && num2 === num3) {
+      alert('Equilateral'); 
+      } else if (num1 === num2 || num2 === num3 || num1 === num3) {
+        alert('Isosceles');
+      } else if (num1 !== num2 && num2 !== num3 && num1 !==num3) {
+        alert('Scalene');
+      } else {
+        alert("Your numbers don't make a triangle. Please try again");
+      }
     }
     
-
     event.preventDefault();
   });
 
---
+++ userstories.md
@@ -0,0 +1,27 @@
+# User stories to fulfill: 
+
+1. If I enter 3 identical numbers, I should get 'Equilateral'
+
+2. If I enter only 2 equal numbers, I should get 'Isosceles.'
+
+3. If I enter no equal numbers (that are still valid) I should get 'Scalene.'
+
+4. If I enter an invalid set of numbers, I should get, "Not a triangle."
+
+#### My steps:
+1. Figure out if it's a triangle
+2. Use branching logic to tell the user what their triangle is
+
+3. Create error messages if user enters numbers less than 1, or greater than the lengths of any other two combines legs
+
+
+Track 2:
+1. If I enter 3 identical numbers, I should get 'Equilateral'
+
+2. If I enter 2 equal numbers that **aren't** greater than the 3rd, I should get 'Isosceles.'
+
+3. If I enter 2 equal numbers that **are** greater than the third, 
+
+(...)
+
+background image: 
